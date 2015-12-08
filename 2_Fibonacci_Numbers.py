d83e47f4cb5c9631681b1e309cab1d00a26092ad
baranmcl <baranmcl@gmail.com>
2015-12-08 14:27:11 -0500

    finished problem
    added fibNumber generator
    added solution

--- 2_Fibonacci_Numbers.py
+++ 2_Fibonacci_Numbers.py
@@ -3,3 +3,20 @@
 #1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million,
 #find the sum of the even-valued terms.
+def fibSum(fibLimit):
+  answer = 0
+  prevFib = 1
+  fibNumbers = [0, 1]
+  while fibNumbers[-1] <= fibLimit:
+    prevFib = fibNumbers[-1] + fibNumbers[-2]
+    fibNumbers.append(prevFib)
+  
+  for i in fibNumbers:
+    if i % 2 == 0:
+      answer += i
+  
+  return answer
+
+
+if __name__ == '__main__':
+  print(fibSum(4000000))

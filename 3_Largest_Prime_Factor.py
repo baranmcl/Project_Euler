766242dda3b68439ef8c2df9bb940e284b23be2e
baranmcl <baranmcl@gmail.com>
2015-12-09 17:47:55 -0500

    solved problem
    added prime checker
    added function

--- 3_Largest_Prime_Factor.py
+++ 3_Largest_Prime_Factor.py
@@ -1,7 +1,25 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
+import math
+
+def isPrime(n):
+    if n == 2:
+        return True
+    if n%2 == 0 or n <= 1:
+        return False
+    sqr = int(math.sqrt(n)) + 1
+    for divisor in range(3, sqr, 2):
+        if n%divisor == 0:
+            return False
+    return True
+
 def largestPrimeFactor(n):
-  return n
+    answer = 0
+    for i in range(1, int(math.sqrt(n))):
+        if n % i == 0:
+            if isPrime(i):
+                answer = i
+    return answer
 
 
 if __name__ == '__main__':

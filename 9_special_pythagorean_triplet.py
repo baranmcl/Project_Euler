cd023cd2e591c4db7bb1626a2987910f2b76afc1
McLean Baran <baranmcl@gmail.com>
2015-12-12 13:09:42 -0500

    solved problem
    Had to cheat on this one…:-(.
    live and learn

--- 9_special_pythagorean_triplet.py
+++ 9_special_pythagorean_triplet.py
@@ -5,10 +5,14 @@
 
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
-def pythagTrip():
-    
-    return 3
+def pythagTrip(n):
+    for i in range(1,n,1):
+        for j in range(1,n-1,1):
+            k = n-i-j
+            if i**2+j**2==k**2:
+                return i*j*k
+    return 0
 
 
 if __name__ == '__main__':
-    print(pythagTrip())
+    print(pythagTrip(1000))

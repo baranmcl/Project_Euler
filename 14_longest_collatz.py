410826ace66040c532d50fd7cf1c69d098f70a8b
baranmcl <baranmcl@gmail.com>
2015-12-16 14:00:23 -0500

    solved problem
    It takes awhile. I do not think this is an optimal solution. Might want to return later to optimize.

--- 14_longest_collatz.py
+++ 14_longest_collatz.py
@@ -7,3 +7,27 @@
 #Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 #Which starting number, under one million, produces the longest chain?
 #NOTE: Once the chain starts the terms are allowed to go above one million.
+def collatzLen(n):
+    count = 1
+    current = n
+    while current > 1:
+        if current % 2 == 0:
+            current = current / 2
+        elif current % 2 == 1:
+            current = 3*current + 1
+        count += 1
+    return count
+
+def longestCollatz(n):
+    answer = 0
+    longest = 0
+    for i in xrange(n):
+        collatz = collatzLen(i)
+        if collatz > longest:
+            longest = collatz
+            answer = i
+    return longest, answer
+
+
+if __name__ == '__main__':
+    print(longestCollatz(1000000))

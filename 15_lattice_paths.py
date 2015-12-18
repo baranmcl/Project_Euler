34a62f9076d132a923b9e81dbbb207027cadeb0d
McLean Baran <baranmcl@gmail.com>
2015-12-17 22:50:44 -0500

    solved problem 15
    this one was a doozy

--- 15_lattice_paths.py
+++ 15_lattice_paths.py
@@ -1,8 +1,20 @@
+# -*- coding: UTF-8 -*-
 #Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
 #there are exactly 6 routes to the bottom right corner.
 #How many such routes are there through a 20×20 grid?
+
 def latticePaths(n):
-  return n
+    matrix = [1] * n
+    for i in range(n):
+        for j in range(i):
+            matrix[j] = matrix[j]+matrix[j-1]
+            #print matrix , i , j
+        matrix[i] = 2 * matrix[i - 1]
+        #print matrix, i
+    return matrix[n-1] #returns the bottom right corner
 
 if __name__ == '__main__':
-  print(latticePaths(20))
+    print(latticePaths(20))
+
+#once again, help was found from Jason's Code Blog.
+#http://code.jasonbhill.com/python/project-euler-problem-15/

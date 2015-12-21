b2fefa408e81f327ce762fb829287e4dc8c6e82a
McLean Baran <baranmcl@gmail.com>
2015-12-20 23:28:52 -0500

    solved problem 18

--- 18_max_path_sum_I.py
+++ 18_max_path_sum_I.py
@@ -1,4 +1,4 @@
-#By starting at the top of the triangle below and moving to adjacent numbers on the row below, 
+#By starting at the top of the triangle below and moving to adjacent numbers on the row below,
 #the maximum total from top to bottom is 23.
 
 #   3
@@ -26,14 +26,35 @@
 #63 66 04 68 89 53 67 30 73 16 69 87 40 31
 #04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 
-#NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. 
-#However, Problem 67, is the same challenge with a triangle containing one-hundred rows; 
+#Note: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+#However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
 #it cannot be solved by brute force, and requires a clever method! ;o)
 
-def maxPathSum(n):
-    return n
+def maxPathSum(table):
+    for row in range(len(table)-1, 0, -1):
+        for col in range(0, row):
+            table[row-1][col] += max(table[row][col], table[row][col+1])
+    return table[0][0]
 
 if __name__ == '__main__':
     triangle = []
-    print(maxPathSum(triangle))
+    triangle.append("75")
+    triangle.append("95 64")
+    triangle.append("17 47 82")
+    triangle.append("18 35 87 10")
+    triangle.append("20 04 82 47 65")
+    triangle.append("19 01 23 75 03 34")
+    triangle.append("88 02 77 73 07 63 67")
+    triangle.append("99 65 04 28 06 16 70 92")
+    triangle.append("41 41 26 56 83 40 80 70 33")
+    triangle.append("41 48 72 33 47 32 37 16 94 29")
+    triangle.append("53 71 44 65 25 43 91 52 97 51 14")
+    triangle.append("70 11 33 28 77 73 17 78 39 68 17 57")
+    triangle.append("91 71 52 38 17 14 91 43 58 50 27 29 48")
+    triangle.append("63 66 04 68 89 53 67 30 73 16 69 87 40 31")
+    triangle.append("04 62 98 27 23 09 70 98 73 93 38 53 60 04 23")
 
+    triangle = [i.split() for i in triangle]
+    triangle = [[int(j) for j in i] for i in triangle]
+
+    print(maxPathSum(triangle))

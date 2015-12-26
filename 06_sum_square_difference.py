e0d0b092918589133cd83ca0aa628235c94c0886
McLean Baran <baranmcl@gmail.com>
2015-12-26 16:06:36 -0500

    condensed solution
    condensed solution to one line of code, because why not?

--- 06_sum_square_difference.py
+++ 06_sum_square_difference.py
@@ -6,13 +6,7 @@
 #and the square of the sum is 3025 − 385 = 2640.
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 def sumSquareDiff(n):
-    numRange = range(1, n+1)
-    sumSquare = (sum(numRange))**2 #square of the sum
-    squareSum = 0 #sum of the squares
-    for i in numRange:
-        squareSum += (i**2)
-        
-    return sumSquare-squareSum
+    return (sum(range(1, n+1)))**2 - (sum([int(i)**2 for i in range(1, n+1)]))
 
 if __name__ == '__main__':
     print(sumSquareDiff(100))
